2a8bc3c0f5ca700ac95085b902e0c827107e2fa2
ChangDae1993 <jsj939639@gmail.com>
2024-09-14 16:24:03 +0900

    Player+Enemy_Move_ADD

---
+++ Assets/Script/TestScript/CameraScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject playerTarget;
+    public Vector3 followPos;
+    public float camSize = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        followPos = new Vector3(playerTarget.transform.position.x, 
+            playerTarget.transform.position.y,
+            -30f);
+
+        Camera.main.orthographicSize = camSize;
+
+        this.transform.position = followPos;
+    }
+}
---
+++ Assets/Script/TestScript/Enemy.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 0f;
+    public Rigidbody2D target;
+
+    public bool islive = false;
+
+    public Rigidbody2D rigid;
+    public SpriteRenderer spriteRen;
+
+    private void Awake()
+    {
+        rigid = this.GetComponent<Rigidbody2D>();
+        spriteRen = this.GetComponent<SpriteRenderer>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!islive)
+            return;
+
+        Vector2 targerDIr = target.position - rigid.position;
+        Vector2 nextVec = targerDIr.normalized * speed * Time.fixedDeltaTime;
+
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.velocity = Vector2.zero;
+    }
+
+    // Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+
+    #region spriteFLip
+
+    //private void LateUpdate()
+    //{
+    //    if (!islive)
+    //        return;
+
+    //    //작으면 false, 크면 true
+    //    spriteRen.flipX = target.position.x < rigid.position.x;
+    //}
+
+    #endregion
+}

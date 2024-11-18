9712f5c4c512d532abcf14cfc0141ff3a04f75cc
ChangDae1993 <jsj939639@gmail.com>
2024-11-18 18:24:37 +0900

    UI_Manager_ADD

---
+++ Assets/Script/Manager_Script/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager GM;
+    [Header("UI Manager")]
+    public UIManager UM;
+    [Space(10f)]
+    public Player player;
+
+
+    private void Awake()
+    {
+        GM = this;
+        Debug.Log("Awake");
+        if (GM == null)
+        {
+            Debug.Log("start");
+            DontDestroyOnLoad(this.gameObject);
+
+            GM = this;
+        }
+        else if (GM != this)
+        {
+            Debug.Log("start2");
+            Destroy(gameObject);
+        }
+        else
+        {
+            Debug.Log("start3");
+            DontDestroyOnLoad(gameObject);
+        }
+
+        UM = this.transform.Find("UIManager").GetComponent<UIManager>();
+
+
+
+#if UNITY_EDITOR
+        Cursor.visible = true;
+#else
+        Cursor.visible = false;
+#endif
+
+    }
+
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}

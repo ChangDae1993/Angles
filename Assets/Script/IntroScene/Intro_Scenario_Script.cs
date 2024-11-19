c927f94835323f10e64b956d6b614d91288d89a8
ChangDae1993 <jsj939639@gmail.com>
2024-11-19 12:03:11 +0900

    Intro_Scene_MODIFY2

--- Assets/Script/IntroScene/Intro_Scenario_Script.cs
+++ Assets/Script/IntroScene/Intro_Scenario_Script.cs
@@ -8,9 +8,22 @@ public class Intro_Scenario_Script : MonoBehaviour
 
     public Text[] storyTexts;
 
+    Coroutine sceneChange;
+    Coroutine textShow;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if(sceneChange != null)
+        {
+            StopCoroutine(sceneChange);
+            sceneChange = StartCoroutine(ScenechngCo(true));
+        }
+        else
+        {
+            sceneChange = StartCoroutine(ScenechngCo(true));
+        }
+
+
         if (textShow != null)
         {
             StopCoroutine(textShow);
@@ -22,7 +35,7 @@ public class Intro_Scenario_Script : MonoBehaviour
         }
     }
 
-    Coroutine textShow;
+
     // Update is called once per frame
     void Update()
     {
@@ -30,7 +43,7 @@ public class Intro_Scenario_Script : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))
             {
-                Debug.Log("no mouse");
+                //Debug.Log("no mouse");
                 return;
             }
 
@@ -53,29 +66,66 @@ public class Intro_Scenario_Script : MonoBehaviour
             }
             else
             {
-                Debug.Log("Next Scene Go");
+                if (sceneChange != null)
+                {
+                    StopCoroutine(sceneChange);
+                    sceneChange = StartCoroutine(ScenechngCo(false));
+                }
+                else
+                {
+                    sceneChange = StartCoroutine(ScenechngCo(false));
+                }
             }
-
-
         }
     }
 
     [Header("text direction")]
     public float fadeInTime;
-    public float fadeOutTime;
-    public float colorAlpha = 0f;
+    private float colortextAlpha = 0f;
     public IEnumerator textShowCo()
     {
-        colorAlpha = 0f;
+        colortextAlpha = 0f;
         while (storyTexts[textindex].color.a < 255f)
         {
-            Debug.Log("Fade In ");
-            colorAlpha += fadeInTime;
-            storyTexts[textindex].color = new Color(0f, 0f, 0f, colorAlpha);
+            //Debug.Log("Fade In ");
+            colortextAlpha += fadeInTime;
+            storyTexts[textindex].color = new Color(0f, 0f, 0f, colortextAlpha);
             yield return new WaitForSeconds(0.1f);
         }
+    }
 
+    public Image SceneCHNGpanel;
+    [SerializeField] private float colorPanelAlpha = 255f;
+    private float fadetime = 0.5f;
+    public IEnumerator ScenechngCo(bool onoff)
+    {
+        if(onoff)
+        {
 
-        //yield return new WaitForSeconds(fadeOutTime);
+            while (colorPanelAlpha > 0f)
+            {
+                colorPanelAlpha -= fadetime;
+                SceneCHNGpanel.color = new Color(0f, 0f, 0f, colorPanelAlpha);
+                Debug.Log(colorPanelAlpha);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            SceneCHNGpanel.color = new Color(0f, 0f, 0f, 0f);
+        }
+        else
+        {
+            //다음 씬으로 넘어가기
+            colorPanelAlpha = 0f;
+            while (colorPanelAlpha < 255f)
+            {
+                SceneCHNGpanel.color = new Color(0f, 0f, 0f, colorPanelAlpha);
+                yield return new WaitForSeconds(0.1f);
+            }
+
+            SceneCHNGpanel.color = new Color(0f, 0f, 0f, 255f);
+            Debug.Log("Next Scene Go");
+        }
+
+        yield return null;
     }
 }

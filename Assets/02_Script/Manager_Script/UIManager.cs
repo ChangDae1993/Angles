3b0d4bcf7734757a641da8fcccd916db2d9af601
ChangDae1993 <jsj939639@gmail.com>
2024-11-19 18:36:48 +0900

    LevelUp_DELETE_(Later)

--- Assets/02_Script/Manager_Script/UIManager.cs
+++ Assets/02_Script/Manager_Script/UIManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-using UnityEngine.UIElements;
+using System.Collections;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -14,6 +15,14 @@ public class UIManager : MonoBehaviour
 
     [Header("Main Scene")]
     public GameObject BtnGroup;
+
+    [Space(10f)]
+
+    [Header("Stage Scene")]
+    public bool windowOnOff = false;
+
+    public Image optionPanel;
+    public bool gamePause;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -23,7 +32,40 @@ public class UIManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (GameManager.GM.UM.stgCNt > (UIManager.stagecount)1)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (!windowOnOff)
+                {
+                    gamePause = true;
+                    OptionShow(true);
+                    Time.timeScale = 0f;
+                    windowOnOff = true;
+                }
+                else
+                {
+                    gamePause = false;
+                    OptionShow(false);
+                    Time.timeScale = 1f;
+                    windowOnOff = false;
+                }
+            }
+        }
+
+    }
+
+
+    public void OptionShow(bool onoff)
+    {
+        if (onoff)
+        {
+            optionPanel.gameObject.SetActive(true);
+        }
+        else
+        {
+            optionPanel.gameObject.SetActive(false);
+        }
     }
 
     public void ExitGame()

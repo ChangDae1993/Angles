ac20fdd3da2dcf7a9b5231ab01a804751ffb0bcd
ChangDae1993 <jsj939639@gmail.com>
2024-11-25 13:38:32 +0900

    img_IMPORT+version_Text_ADD

---
+++ Assets/02_Script/IntroScene/versionText.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class versionText : MonoBehaviour
+{
+
+    public Text versionTxt;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        versionTxt.text = "ver." + Application.version.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
